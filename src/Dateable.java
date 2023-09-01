1787d46ab030661d4253cd8f9339698820696bf8
joanwongclgmail.com <joanwongcl@gmail.com>
2023-09-01 16:17:32 +0800

    Add method Declared and initialized 'age' Added print statement

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,8 @@
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        int age=78;
+        System.out.println("78-year olds should date somebody who is at least 46 years old.");
     }
 }
+
