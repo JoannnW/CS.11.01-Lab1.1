d4058163ff63f00e462740ec35389e5a1cb7b248
joanwongclgmail.com <joanwongcl@gmail.com>
2023-09-04 15:53:33 +0800

    Add method Declared and initialized 'age' Added scanner print statement

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,8 +1,36 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        int age=78;
-        System.out.println("78-year olds should date somebody who is at least 46 years old.");
+        Scanner ageScanner = new Scanner(System.in);
+        System.out.println("Please enter your age:");
+        int age =-1;
+        while (age < 0) {
+            try {
+                age = Integer.parseInt(ageScanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number.");
+            }
+        }
+        age=10;
+        while (age < 11) {
+            try {
+                age = Integer.parseInt(ageScanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("You're too young to date");
+            }
+        }
+        System.out.println("Age: " + age);
+        int ageHalved = age / 2;
+        int ageHalvedPlusSeven = ageHalved + 7;
+                System.out.println("You should date someone who's at least");
+                System.out.println(ageHalvedPlusSeven);
+                System.out.println("years old");
+            }
+
     }
-}
+
+
+
+
 
